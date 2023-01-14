5809707f77b99fdcef1c55909484bc9d1c57c559
Bruno Baptista Pereira <bruno.baptistapereira@gmail.com>
2023-01-13 23:40:49 -0300

    Second commit

---
+++ CadastroContatos/Models/ContatoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroContatos.Models
+{
+    public class ContatoModel
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Digite o nome do contato")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o sobrenome do contato")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Digite o email do contato")]
+        [EmailAddress(ErrorMessage ="O e-mail informado não é valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o celular do contato")]
+        [Phone(ErrorMessage = "O celular informado não é valido")]
+        public string Celular { get; set; }
+    }
+}
---
+++ CadastroContatos/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using CadastroContatos.Models;
+using System.Collections.Generic;
+
+namespace CadastroContatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel ListarPorId(int id);
+
+        List<ContatoModel> BuscarTodos();
+
+        ContatoModel Adicionar(ContatoModel contato);
+
+        ContatoModel Atualizar(ContatoModel contato);
+
+        bool Apagar(int id);
+    }
+}
